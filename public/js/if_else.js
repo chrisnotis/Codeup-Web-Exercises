e886ea541447c2bda3ea2ef7d13f306c4f09a5d5
Christopher Harman <chris@Christophers-MacBook-Pro.local>
2016-01-18 20:21:47 -0600

    Javascript classwork - working on arrays, iterations of arrays and manipulating arrays

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -1,9 +1,14 @@
 // ignore these lines for now
 // just know that the variable 'color' will end up with a random value from the colors array
+
+
+
 // var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 // var color = colors[Math.floor(Math.random()*colors.length)];
 
 // var favorite = 'blue'; // TODO: change this to your favorite color from the list
+
+
 
 // TODO: Create a block of if/else statements to check for every color except indigo and violet.
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
